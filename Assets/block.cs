0c6c2694afb06aea4cda6d555256dd503ad92a7d
Connor Cone <connor@sabrient.ai>
2021-10-20 22:34:30 -0500

    projectile basics working

--- Assets/block.cs
+++ Assets/block.cs
@@ -13,7 +13,7 @@ public class block : MonoBehaviour
     void Block()
     {
         Debug.Log("Sprite Clicked at :" + blockStart);
-        this.GetComponent<SpriteRenderer>().sprite = blockSprite;
+    //    this.GetComponent<SpriteRenderer>().sprite = blockSprite;
         blockStart = Time.frameCount;
         this.state = State.Blocking;
     }
@@ -22,16 +22,26 @@ public class block : MonoBehaviour
             Block();
     }
 
+    void OnTriggerEnter2D(Collider2D coll){
+        // Literally all possible collisions are projectiles that are either blocked or kill you
+        Debug.Log("Bitch might have been hit?");
+        if (this.state != State.Blocking)
+        {
+            // you lose floors, bitch
+            Debug.Log("bitch got hit");
+        }
+
+    }
+
     public void Update()
     {
         if (this.state == State.Blocking)
         {
-            Debug.Log("we blockin");
             if (Time.frameCount - blockStart > 120)
             {
                 Debug.Log("yo, time to stand");
                 this.state = State.Standing;
-                this.GetComponent<SpriteRenderer>().sprite = standSprite;
+     //           this.GetComponent<SpriteRenderer>().sprite = standSprite;
             }
         }
     }
